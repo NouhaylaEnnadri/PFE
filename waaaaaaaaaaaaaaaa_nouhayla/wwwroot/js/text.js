748a8b135142852349a9db1d89fefe1afa2ffaaf
Nouhayla <nouhayla.ennadri@usmba.ac.ma>
2023-02-05 13:22:34 +0100

    the most basic text editor

--- waaaaaaaaaaaaaaaa_nouhayla/wwwroot/js/text.js
+++ waaaaaaaaaaaaaaaa_nouhayla/wwwroot/js/text.js
@@ -8,16 +8,10 @@ connection.start().catch(err => console.error(err));
 connection.on("ReceiveText", (text) => {
     editor.value = text;
     editor.focus();
-    editor.setSelectionRange(editor.value.length, editor.value.length);
+   editor.setSelectionRange(editor.value.length, editor.value.length);
 });
 
 function change() {
     connection.invoke("BroadcastText", editor.value).catch(err => console.error(err));
 }
 
-editor.style.display = "none";
-var group = document.getElementById("group");
-function join() {
-    connection.invoke("JoinGroup", group.value).catch(err => console.error(err));
-    editor.style.display = "initial";
-}
