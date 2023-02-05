3ffcebcd2007766fd72896fb3e4c392ec8654bd0
Nouhayla <nouhayla.ennadri@usmba.ac.ma>
2023-02-05 13:51:17 +0100

    text editor fonctionne 50%

---
+++ waaaaaaaaaaaaaaaa_nouhayla/Models/texteditor.cs
@@ -0,0 +1,10 @@
+﻿namespace pfe.Models
+{
+    public class texteditor
+    {
+    
+            public string Title { get; set; }  // for the title of the text
+            public string Body { get; set; }  // for the body of the text
+        }
+    
+}
